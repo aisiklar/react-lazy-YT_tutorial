2b7cffa5d582cf71b402fcdd6db15d7d2005bbd8
Mehmet Alptekin Isiklar <maisiklar@gmail.com>
2024-01-17 13:24:50 +0300

    new App file for use with and without LazyLoading

--- lazy-completed/src/App.jsx
+++ lazy-completed/src/App.jsx
@@ -1,38 +1,26 @@
-import { Routes, Route, useNavigate } from "react-router-dom"
-import Home from "./components/Home"
-import Layout from "./components/Layout"
-import { ErrorBoundary } from "react-error-boundary"
-import ErrorFallback from "./components/ErrorFallback"
-import SkeletonAdmin from './components/skeletons/SkeletonAdmin'
-import { lazy, Suspense } from "react"
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./components/ErrorFallback";
+import SkeletonAdmin from "./components/skeletons/SkeletonAdmin";
+import { lazy, Suspense } from "react";
 
-const Admin = lazy(() => import('./components/Admin'))
+//const Admin = lazy(() => import('./components/Admin'))
+import Admin from "./components/Admin";
 
 function App() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <Routes>
-
       <Route path="/" element={<Layout />}>
-
         <Route index element={<Home />} />
 
-        <Route path="admin" element={
-          <ErrorBoundary
-            FallbackComponent={ErrorFallback}
-            onReset={() => navigate('/')}
-          >
-            <Suspense fallback={<SkeletonAdmin />}>
-              <Admin />
-            </Suspense>
-          </ErrorBoundary>}
-        />
-
+        <Route path="admin" element={<Admin />}></Route>
       </Route>
-
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
